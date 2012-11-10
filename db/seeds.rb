6d460bf6f73148420bc8ac604d72cda76ee00987
dhonig <daniel.honig@gmail.com>
2012-11-10 14:50:58 -0500

    it appears that this attribute has been changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ if Spree::Product.gift_cards.count == 0
   [25, 50, 75, 100].each do |value|
     option_value = Spree::OptionValue.new(name: value, presentation: "$#{value}")
     option_value.option_type = option_type
-    variant = Spree::Variant.new(price: value.to_i, sku: "GIFTCERT#{value}", count_on_hand: 1000)
+    variant = Spree::Variant.new(price: value.to_i, sku: "GIFTCERT#{value}", on_hand: 1000)
     variant.option_values << option_value
     product.variants << variant
   end
