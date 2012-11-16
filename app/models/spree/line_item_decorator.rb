89cd183e80e5e98661a0d6f8b84475739e030823
Jeff Dutil <jdutil@burlingtonwebapps.com>
2012-11-16 03:03:45 -0500

    Improve LineItem decorator.

--- app/models/spree/line_item_decorator.rb
+++ app/models/spree/line_item_decorator.rb
@@ -1,7 +1,8 @@
 Spree::LineItem.class_eval do
 
-  has_one :gift_card
+  has_one :gift_card, dependent: :destroy
 
   validates :gift_card, presence: { if: Proc.new{ |item| item.product.is_gift_card? } }
+  validates :quantity,  numericality: { if: Proc.new{ |item| item.product.is_gift_card? }, less_than_or_equal_to: 1 }
 
 end
