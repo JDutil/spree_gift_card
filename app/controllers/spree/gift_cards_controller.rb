79a516edac85db233bb62c63e949965a0cc502c5
Dries Steenhouwer <steenhouwer.dries@gmail.com>
2012-11-29 11:47:23 +0100

    fix for latest spree in master

--- app/controllers/spree/gift_cards_controller.rb
+++ app/controllers/spree/gift_cards_controller.rb
@@ -38,7 +38,7 @@ module Spree
 
     def find_gift_card_variants
       gift_card_product_ids = Product.not_deleted.where(["is_gift_card = ?", true]).map(&:id)
-      @gift_card_variants = Variant.where(["price > 0 AND product_id IN (?)", gift_card_product_ids]).order("price")
+      @gift_card_variants = Variant.joins(:prices).where(["amount > 0 AND product_id IN (?)", gift_card_product_ids]).order("amount")
     end
 
   end
