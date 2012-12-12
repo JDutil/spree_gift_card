bad0940c9458a040630e1bdda735bd85f2e3c471
Jeff Dutil <jdutil@burlingtonwebapps.com>
2012-12-12 17:08:20 -0500

    Use #try when checking if product is gift card.

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -5,7 +5,7 @@ Spree::ProductsController.class_eval do
   private
 
     def redirect_gift_card
-      redirect_to new_gift_card_path and return false if @product.is_gift_card?
+      redirect_to new_gift_card_path and return false if @product.try :is_gift_card?
     end
 
 end
