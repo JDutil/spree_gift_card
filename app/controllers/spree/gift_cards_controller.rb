9eeb18916dbe1e3a5fcc9253ee75b97b7b49ea5e
jonb <jonb@lanl.gov>
2012-12-26 16:06:00 -0700

    Fix conflict cherry picking 1-3 branch fix.

--- app/controllers/spree/gift_cards_controller.rb
+++ app/controllers/spree/gift_cards_controller.rb
@@ -37,7 +37,7 @@ module Spree
     private
 
     def find_gift_card_variants
-      gift_card_product_ids = Product.not_deleted.where(["is_gift_card = ?", true]).map(&:id)
+      gift_card_product_ids = Product.not_deleted.where(is_gift_card: true).pluck(:id)
       @gift_card_variants = Variant.joins(:prices).where(["amount > 0 AND product_id IN (?)", gift_card_product_ids]).order("amount")
     end
 
