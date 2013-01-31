83b39362b03c864c946048b0af4442c923f3b055
Ruby <jdutil@burlingtonwebapps.com>
2013-01-30 22:08:41 -0800

    Fix uninitialized constant Calculator occurring in development.

--- lib/spree_gift_card/engine.rb
+++ lib/spree_gift_card/engine.rb
@@ -15,6 +15,9 @@ module SpreeGiftCard
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/models/spree/calculator/*.rb")) do |c|
+        Rails.env.production? ? require(c) : load(c)
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
