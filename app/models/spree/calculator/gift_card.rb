57615c609b7761ba37b1edf46ee747fc824b5c79
Ruby <jdutil@burlingtonwebapps.com>
2013-01-30 22:08:41 -0800

    Fix uninitialized constant Calculator occurring in development.

--- app/models/spree/calculator/gift_card.rb
+++ app/models/spree/calculator/gift_card.rb
@@ -1,13 +1,18 @@
+require_dependency 'spree/calculator'
+
 module Spree
-  class Calculator::GiftCard < Calculator
+  class Calculator < ActiveRecord::Base
+    class GiftCard < Calculator
 
-    def self.description
-      'Gift Card Calculator'
-    end
+      def self.description
+        'Gift Card Calculator'
+      end
 
-    def compute(order, gift_card)
-      # Ensure a negative amount which does not exceed the sum of the order's item_total, ship_total, and tax_total.
-      [(order.item_total + order.ship_total + order.tax_total), gift_card.current_value].min * -1
+      def compute(order, gift_card)
+        # Ensure a negative amount which does not exceed the sum of the order's item_total, ship_total, and tax_total.
+        [(order.item_total + order.ship_total + order.tax_total), gift_card.current_value].min * -1
+      end
+
     end
   end
 end
