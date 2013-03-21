de4e87b7da7408ebd22a3e4a183e9d815ef27b7e
Rocco Stanzione <grasshopper@linuxkungfu.org>
2013-03-21 13:23:41 -0500

    Better support for async mailers

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,9 +1,9 @@
 Spree::OrderMailer.class_eval do
-  def gift_card_email(card, order)
-    @gift_card = card
-    @order = order
+  def gift_card_email(card_id, order_id)
+    @gift_card = Spree::GiftCard.find(card_id)
+    @order = Spree::Order.find(order_id)
     subject = "#{Spree::Config[:site_name]} Gift Card"
     @gift_card.update_attribute(:sent_at, Time.now)
-    mail(:to => card.email, :subject => subject)
+    mail(:to => @gift_card.email, :subject => subject)
   end
 end
