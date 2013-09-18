3c7cbf044d812b999e6b5f4b02943024b59ee3ee
Jeff Dutil <jdutil@burlingtonwebapps.com>
2013-09-18 18:04:26 -0400

    Update durable_decorator depedency. Fixes #27

--- app/models/spree/stock/quantifier_decorator.rb
+++ app/models/spree/stock/quantifier_decorator.rb
@@ -2,9 +2,7 @@ module Spree
   module Stock
     Quantifier.class_eval do
       durably_decorate :can_supply?, mode: 'soft', sha: '2759d397d8dafe5bc3e5eed2d881fa0ab3a1a7c9' do |required|
-        # TODO can use original method when my pr gets merged to durable_decorator
-        # original_can_supply?(required) || Spree::Variant.find(@variant).product.is_gift_card?
-        total_on_hand >= required || backorderable? || Spree::Variant.find(@variant).product.is_gift_card?
+        original_can_supply?(required) || Spree::Variant.find(@variant).product.is_gift_card?
       end
     end
   end
