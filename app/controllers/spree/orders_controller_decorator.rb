e1f290476c2712117efa9e53ff6eaf05a109552e
Chris Schmitz <ccschmitz@gmail.com>
2013-10-07 13:11:56 -0500

    Update decorator SHAs

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,6 +1,6 @@
 Spree::OrdersController.class_eval do
 
-  durably_decorate :update, mode: 'soft', sha: '91961146778f771e81540197e030dcf4cc1d99df' do
+  durably_decorate :update, mode: 'soft', sha: 'f2e5c56e5b4b0eccb62af6571c1c150da3cf334e' do
     @order = current_order
     if @order.update_attributes(params[:order])
       render :edit and return unless apply_coupon_code if defined?(Spree::Promo)
