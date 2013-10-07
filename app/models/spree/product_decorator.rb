ca8170c113f3946f7ed7d12608067610437a6172
Chris Schmitz <ccschmitz@gmail.com>
2013-10-07 14:30:24 -0500

    Use new scope syntax

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Product.class_eval do
 
-  scope :gift_cards, where(is_gift_card: true)
-  scope :not_gift_cards, where(is_gift_card: false)
+  scope :gift_cards, -> { where(is_gift_card: true) }
+  scope :not_gift_cards, -> { where(is_gift_card: false) }
 
 end
