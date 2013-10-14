832227a30e4dc83f642aee806badc20a6d76c774
Chris Schmitz <ccschmitz@gmail.com>
2013-10-13 23:06:20 -0500

    Make gift codes shorter so they are easier to type in

--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -59,7 +59,7 @@ module Spree
 
     def generate_code
       until self.code.present? && self.class.where(code: self.code).count == 0
-        self.code = Digest::SHA1.hexdigest([Time.now, rand].join)
+        self.code = SecureRandom.hex
       end
     end
 
