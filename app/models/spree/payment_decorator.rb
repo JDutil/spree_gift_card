34f50fc277a1be7bf1fa0ad36c8058a042d6c58d
geermc4 <geermc4@gmail.com>
2013-12-10 12:16:27 -0800

    send email when payment is captured

---
+++ app/models/spree/payment_decorator.rb
@@ -0,0 +1,9 @@
+Spree::Payment.class_eval do
+  before_update :send_gift_card, :if => "state_changed? && state_was != 'completed' && state == 'completed'"
+
+  def send_gift_card
+    order.line_items.each do |li|
+      Spree::OrderMailer.gift_card_email(li.gift_card.id, order).deliver if li.gift_card
+    end
+  end
+end
