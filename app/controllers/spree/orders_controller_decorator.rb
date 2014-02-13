ee9f2c2b346f50266e7cc00d6bd31a025ab611fd
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-02-13 23:09:17 +0000

    Simplify method.

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,25 +1,8 @@
 Spree::OrdersController.class_eval do
 
-  durably_decorate :update, mode: 'soft', sha: 'b5f4112738f931e0aa0b1b6cc4071c27d9d5b0a0' do
-    @order = current_order
-    if @order.update_attributes(params[:order])
-      render :edit and return unless apply_coupon_code if defined?(Spree::Promo)
-      render :edit and return unless apply_gift_code
-
-      @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
-      fire_event('spree.order.contents_changed')
-      respond_with(@order) do |format|
-        format.html do
-          if params.has_key?(:checkout)
-            redirect_to checkout_state_path(@order.checkout_steps.first)
-          else
-            redirect_to cart_path
-          end
-        end
-      end
-    else
-      respond_with(@order)
-    end
+  durably_decorate :after_update_attributes, mode: 'soft', sha: '4a4bdc54d88160bed9de2b5ae64d9f5715cbf99a' do
+    apply_gift_code
+    original_after_update_attributes
   end
 
 end
