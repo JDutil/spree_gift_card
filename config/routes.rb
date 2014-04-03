f31bca0c2f3a9204f9da069aa013ecf63aa43ec6
Richard Wilson <richard@freerunningtech.com>
2014-04-03 09:42:16 -0700

    Add a button to void gift cards.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Spree::Core::Engine.routes.draw do
   resources :gift_cards
   namespace :admin do
-    resources :gift_cards
+    resources :gift_cards do
+      member do
+        put :void
+      end
+    end
   end
 end
