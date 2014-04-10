19cb289d75558ce0736b2b82fc98573584e893e9
Dylan Kendal <dkendal@freerunningtechnologies.com>
2014-04-10 11:42:23 -0700

    paginate @gift_cards on user view
    Change-Id: I547b62dedbce523d92d172d1bf01c006bf5189f2

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -1,8 +1,8 @@
 Spree::UsersController.class_eval do
   def gift_cards
     @show_all = params[:show_all] == "true"
-    @gift_cards = current_spree_user.gift_cards.order(:expiration_date).
-      reverse_order
+    @gift_cards = current_spree_user.gift_cards.page(params[:page]).
+      order(:expiration_date).reverse_order
     @gift_cards = @gift_cards.active unless @show_all
   end
 end
