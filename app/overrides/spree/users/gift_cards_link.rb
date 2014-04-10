ed75d8ee940a4a8985b74177357f82161fefc99e
Dylan Kendal <dkendal@freerunningtechnologies.com>
2014-04-10 10:23:33 -0700

    Add a link in users#show to view giftcards
    Change-Id: I9d7e530e171dbf19c719ef98eb552112654d646e

---
+++ app/overrides/spree/users/gift_cards_link.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(virtual_path: "spree/users/show",
+                     name: "add_link_to_gift_cards",
+                     insert_bottom: "[data-hook='account_summary']",
+                     text: <<ERB)
+<%= link_to Spree.t("view_my_gift_certificates"), account_gift_cards_path %>
+ERB
