a270a84a615e20269dffa392674f1292114660fc
Richard Wilson <richard@freerunningtech.com>
2014-04-10 16:35:35 -0700

    Add a button to see the users gift cards.

--- app/overrides/spree/admin/users/show.rb
+++ app/overrides/spree/admin/users/show.rb
@@ -3,6 +3,6 @@ if defined? Devise
     virtual_path: "spree/admin/users/show",
     name: "add_create_gift_card_button_to_show",
     insert_after: "table",
-    partial: "spree/admin/users/new_gift_card"
+    partial: "spree/admin/users/gift_card_buttons"
   )
 end
