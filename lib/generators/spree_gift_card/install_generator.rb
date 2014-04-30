9b6f6040db4c4d5516e668b1e3bec90929438e20
Gregor MacDougall <gregor@freerunningtechnologies.com>
2014-04-30 10:37:02 -0700

    Fix 2.2 installer incompatibility.
    Spree stores now serve up application.css/js instead of splitting them
    in to store and admin.

--- lib/generators/spree_gift_card/install_generator.rb
+++ lib/generators/spree_gift_card/install_generator.rb
@@ -3,13 +3,13 @@ module SpreeGiftCard
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_gift_card\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_gift_card\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require store/spree_gift_card\n"
+        append_file "vendor/assets/javascripts/spree/backend/all.js", "//= require admin/spree_gift_card\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_gift_card\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", " *= require admin/spree_gift_card\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require store/spree_gift_card\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/backend/all.css", " *= require admin/spree_gift_card\n", :before => /\*\//, :verbose => true
       end
 
       def add_migrations
