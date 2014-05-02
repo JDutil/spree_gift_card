29e721e2eb603a18cb11964eb76e5290648109b8
Gregor MacDougall <gregor@freerunningtechnologies.com>
2014-05-02 14:08:14 -0700

    Add a feature spec for cart total.

---
+++ spec/features/add_to_card_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Add to Cart", js: true do
+  let(:product) { create :product, is_gift_card: true }
+  let!(:variant) { create :variant, product: product }
+
+  before do
+    visit spree.root_path
+    click_link 'Buy gift card'
+    fill_in 'gift_card[email]', with: 'test@email.com'
+    fill_in 'gift_card[name]', with: 'nom nom'
+    click_button 'Add To Cart'
+  end
+
+  it 'has the correct total' do
+    expect(page.find('.cart-total').text).should eq 'Total $19.99'
+  end
+
+end
