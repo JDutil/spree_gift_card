b823fbf12ea5744eac2d13af0f1b1596f96ff4d5
Tom Russell <tom@cishub.co.uk>
2015-02-27 15:42:52 +0000

    Update checkout decorator for Spree 2.4
    The checkout controller has changed between spree 2.2 -> 2.4, in that
    the address parameters now appear to be part of the order details rather
    than having to persist it separately.
    
    The commit that changed it in spree is:
    https://github.com/spree/spree/commit/7c16987c822b1ca97e2b3f781aa991e1db9697ec#diff-41759f421fa5ee0d427fec7411738b50

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,13 +2,13 @@ Spree::CheckoutController.class_eval do
 
   Spree::PermittedAttributes.checkout_attributes << :gift_code
 
-  durably_decorate :update, mode: 'soft', sha: '908d25b70eff597d32ddad7876cd89d3683d6734' do
+  durably_decorate :update, mode: 'soft', sha: '9774bcd294f748bd6185a0c8f7d518e624b99ff7' do
     if @order.update_from_params(params, permitted_checkout_attributes)
       if @order.gift_code.present?
         render :edit and return unless apply_gift_code
       end
 
-      persist_user_address
+      @order.temporary_address = !params[:save_user_address]
       unless @order.next
         flash[:error] = @order.errors.full_messages.join("\n")
         redirect_to checkout_state_path(@order.state) and return
