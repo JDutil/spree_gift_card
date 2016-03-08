fabd7f20abf00afe59abf0c318a1d28cad1884c2
sawan gupta <sawan.gupta92@gmail.com>
2016-03-08 18:31:57 +0530

    remove return statements from #apply_gift_card

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -3,10 +3,14 @@ Spree::OrdersController.class_eval do
   before_action :assign_order_with_lock, only: [:apply_gift_card, :update]
 
   def apply_gift_card
-    @order.gift_code = params[:order][:gift_code]
+    @order.assign_attributes(order_gift_code_params)
     apply_gift_code if @order.gift_code.present?
 
     redirect_to checkout_state_path(@order.state)
   end
 
+  private
+    def order_gift_code_params
+      params.require(:order).permit(:gift_code)
+    end
 end
--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -10,10 +10,8 @@ Spree::StoreController.class_eval do
       gift_card = Spree::GiftCard.find_by(code: @order.gift_code)
       if gift_card && gift_card.order_activatable?(@order) && gift_card.apply(@order)
         flash[:success] = Spree.t(:gift_code_applied)
-        return true
       else
         flash[:error] = Spree.t(:gift_code_not_found)
-        return false
       end
     end
 
